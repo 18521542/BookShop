fe068ef210e0901b614a5c0f74f96e18a5d40665
Duy Trong <tranduytrong2000@gmail.com>
2021-11-30 14:58:30 +0700

    add: get user info from register screen

--- sreens/SubUserScreens/RegisterScreen.js
+++ sreens/SubUserScreens/RegisterScreen.js
@@ -3,39 +3,71 @@ import { StyleSheet, Text, View } from 'react-native'
 import { Input } from 'react-native-elements/dist/input/Input'
 import { ScrollView } from 'react-native-gesture-handler'
 import MyButton from '../../components/MyButton'
+import { useState, useEffect } from 'react'
 
 const RegisterScreen = (props) => {
 
-    const register = () => {
-        console.log("Register!!")
+    const [Username, setUsername] = useState("")
+    const [Password, setPassword] = useState("")
+    const [ImageLink, setImglink] = useState("")
+    const [Name, setName] = useState("")
+    const [Telephone, setTelephone] = useState("")
+    const [address, setAddress] = useState("")
+    const [Email, setEmail] = useState("")
+
+    const userInfo = {
+        Username,
+        Password,
+        ImageLink,
+        Name,
+        Telephone,
+        address,
+        Email
+    }
+    const register = ()=>{
+        console.log(userInfo)
     }
     return (
         <ScrollView style={styles.container}>
             <Text>Username</Text>
-            <Input/>
+            <Input  onChangeText={setUsername}/>
+
             <Text>Password</Text>
-            <Input/>
+            <Input onChangeText={setPassword}/>
+
             <Text>Image Link</Text>
-            <Input/>
+            <Input onChangeText={setImglink}/>
+
             <Text>Name</Text>
-            <Input/>
+            <Input onChangeText={setName}/>
+
             <Text>telephone</Text>
-            <Input/>
+            <Input onChangeText={setTelephone}/>
+
             <Text>address</Text>
-            <Input/>
+            <Input onChangeText={setAddress}/>
+
             <Text>email</Text>
-            <Input/>
-            <MyButton
-                width={260}
-                height={40}
-                backgrColor="red"
-                title="Register"
-                onClick={register}
-            />
+            <Input onChangeText={setEmail}/>
+
+            <View style={styles.btn}>
+                <MyButton
+                    width={260}
+                    height={40}
+                    backgrColor="red"
+                    title="Register"
+                    onClick={register}
+                />
+            </View>
+
         </ScrollView>
     )
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    btn:{
+        alignSelf:"center"
+    }
+})
