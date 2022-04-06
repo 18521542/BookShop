6983f6b334c0963bc2c83d7de9d26b47a79524ab
18521542 <tranduytrong2000@gmail.com>
2022-04-06 12:18:16 +0700

    add load more to load category and author

--- src/components/Filter/FilterColumn.js
+++ src/components/Filter/FilterColumn.js
@@ -1,18 +1,37 @@
-import React, { useCallback } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FlatList } from 'react-native'
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, ActivityIndicator } from 'react-native'
 import { ScrollView } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { constant } from '../../constant'
 import { FilterBookByAuthor } from '../../store/actions/author'
 import { FilterBookByCATEGORY } from '../../store/actions/category'
+import { API_ENDPOINT } from '../../constant'
 
 const FilterColumn = (props) => {
-    const { typeScreen, data } = props;
+    const { typeScreen } = props;
+
+    const [loading, setLoading] = useState(false);
+    const [authorData, setAuthor] = useState([]);
+    const [categoryData, setCategory] = useState([]);
+    const [dataSource, setDataSource] = useState([]);
+    const [page, setPage] = useState(1);
 
     const author = useSelector(state => state.author)
     const category = useSelector(state => state.category)
+
+    useEffect(() => {
+        getData("category", setCategory, categoryData);
+        getData("author", setAuthor, authorData);
+    }, [])
+    useEffect(() => {
+        if(typeScreen===constant.bookByAuthorScreenName){
+            setDataSource(authorData)
+        }
+        else
+            setDataSource(categoryData)
+    }, [authorData, categoryData])
 
     const checkIsChosen = (id)=> {
         if(typeScreen===constant.bookByAuthorScreenName){
@@ -29,8 +48,8 @@ const FilterColumn = (props) => {
     const renderTile = (tile) => {
         const dispatch = useDispatch();
         return (
-            <TouchableOpacity 
-                style={styles.tile} 
+            <TouchableOpacity
+                style={styles.tile}
                 key={tile.id}
                 onPress={ async () => {
                     if(typeScreen===constant.bookByAuthorScreenName){
@@ -47,10 +66,46 @@ const FilterColumn = (props) => {
             </TouchableOpacity>
         )
     }
-    const renderList = (data == undefined) ? null : data.map(tile => renderTile(tile))
+
+    const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
+        const paddingToBottom = 20;
+        return layoutMeasurement.height + contentOffset.y >=
+          contentSize.height - paddingToBottom;
+    };
+
+    const getData = (dataType, setState, state) => {
+        setLoading(true);
+        let url = API_ENDPOINT + `/api/${dataType}?page=${page}`
+        fetch(url)
+            .then(res => res.json())
+            .then(resJson => {
+                setPage(page + 1)
+                setState([...state, ...resJson])
+                setLoading(false)
+            })
+            .catch(err => {
+                console.log(err)
+                setLoading(false)
+            })
+    }
+
+    const renderList = (dataSource == undefined) ? null : dataSource.map(tile => renderTile(tile))
     return (
-            <ScrollView>
+            <ScrollView
+                onScroll={({nativeEvent}) => {
+                    if (isCloseToBottom(nativeEvent)) {
+                        if(typeScreen===constant.bookByAuthorScreenName){
+                            getData("author", setAuthor, authorData);
+                        }
+                        else {
+                            getData("category", setCategory, categoryData);
+                        }
+                    }
+                }}
+                scrollEventThrottle={400}
+            >
                 {renderList}
+                {loading ? (<ActivityIndicator/>) : (null)}
             </ScrollView>
     )
 }
